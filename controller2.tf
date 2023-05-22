a2f78ee34a63da797fb88477c40e3e3730dff40f
Nico Marino <dmarino@aviatrix.com>
2023-05-22 08:29:53 +0100

    setting new controller

--- controller2.tf
+++ controller2.tf
@@ -1,5 +1,6 @@
 module "aviatrix-controller-build2" {
-  source            = "git::https://github.com/domenicomarinoaviatrix/aws-cicd-pipeline.git//aviatrix-controller-build"
+  #source            = "git::https://github.com/domenicomarinoaviatrix/aws-cicd-pipeline.git//aviatrix-controller-build"
+  source            = "git::https://github.com/domenicomarinoaviatrix/aws-cicd-pipeline/tree/91e1b602412d43594028b1f248a4688bd659eebe/aviatrix-controller-build"
   vpc               = "vpc-0a0d30848ef411cb7"
   subnet            = "subnet-0698a0316aa277f74"
   keypair           = "Niko_Controller"

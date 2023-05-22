09b4ce38479d3c79c995bcb81a9ea8da5d500ace
Nico Marino <dmarino@aviatrix.com>
2023-05-22 06:57:11 +0100

    setting new controller

--- controller.tf
+++ controller.tf
@@ -11,14 +11,25 @@ terraform {
 
 data "aws_caller_identity" "current" {}
 
-module "aviatrix-iam-roles" {
-  source = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-iam-roles?ref=master"
-}
-
 /*module "aviatrix-iam-roles" {
-  source = "git::https://github.com/domenicomarinoaviatrix/aws-cicd-pipeline.git//aviatrix-controller-iam-roles"
+  source = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-iam-roles?ref=master"
 }*/
 
+module "aviatrix-iam-roles" {
+  source = "git::https://github.com/domenicomarinoaviatrix/aws-cicd-pipeline.git//aviatrix-controller-iam-roles"
+}
+
+
+/*module "aviatrix-controller-build" {
+  source            = "git::https://github.com/domenicomarinoaviatrix/aws-cicd-pipeline.git//aviatrix-controller-build"
+  vpc               = var.vpc_controller
+  subnet            = var.subnet_controller
+  keypair           = var.keypair_name_controller
+  type              = var.type
+  controller_name   = "NewController"
+  ec2role           = module.aviatrix-iam-roles.aviatrix-role-ec2-name
+  incoming_ssl_cidr = ["10.20.30.0/24","0.0.0.0/0"] 
+}*/
 
 module "aviatrix-controller-build" {
   source            = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-build?ref=master"
